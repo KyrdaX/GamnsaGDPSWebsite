51aeb0eb8b7ca3dcf715d3112ba098d927bde242
KyrdaX <137437365+KyrdaX@users.noreply.github.com>
2025-07-06 00:20:30 +0200

    Update Dynamic_Top_Labels_Generator.js

--- js/Dynamic_Top_Labels_Generator.js
+++ js/Dynamic_Top_Labels_Generator.js
@@ -18,7 +18,7 @@ for (let i in levelsToGenerate) {
   imageDiv.className = "image";
 
   const img = document.createElement("img");
-  img.src = "/img/Extreme_Demon_Face.png";
+  img.src = "/GamnsaGDPSWebsite/img/Extreme_Demon_Face.png";
 
   imageDiv.appendChild(img);
 
