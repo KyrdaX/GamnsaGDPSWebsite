15e0aa176e7002a7112995007ba3cdbfbe684abc
KyrdaX <137437365+KyrdaX@users.noreply.github.com>
2025-07-06 03:04:32 +0200

    Update Dynamic_Top_Labels_Generator.js

--- js/Dynamic_Top_Labels_Generator.js
+++ js/Dynamic_Top_Labels_Generator.js
@@ -1,5 +1,5 @@
 const levelsToGenerate = [
-    { name: "Extremescapes", top: 1 },
+    { name: "Extremescapes", top: 1, id: "12345" },
 ];
 
 const container = document.querySelector(".levels_container");
@@ -7,26 +7,29 @@ const container = document.querySelector(".levels_container");
 levelsToGenerate.sort((a, b) => a.top - b.top);
 
 for (let i in levelsToGenerate) {
-  const level = levelsToGenerate[i];
+    const level = levelsToGenerate[i];
+    
+    // Crear el contenedor principal
+    const levelDiv = document.createElement("div");
+    levelDiv.className = "level";
+    
+    // Contenedor de imagen
+    const imageDiv = document.createElement("div");
+    imageDiv.className = "image";
+    
+    const img = document.createElement("img");
+    img.src = "/GamnsaGDPSWebsite/img/Extreme_Demon_Face.png";
+    
+    imageDiv.appendChild(img);
+    
+    const title = document.createElement("h1");
+    title.textContent = `#${level.top} ${level.name}`;
 
-  // Crear el contenedor principal
-  const levelDiv = document.createElement("div");
-  levelDiv.className = "level";
-
-  // Contenedor de imagen
-  const imageDiv = document.createElement("div");
-  imageDiv.className = "image";
-
-  const img = document.createElement("img");
-  img.src = "/GamnsaGDPSWebsite/img/Extreme_Demon_Face.png";
-
-  imageDiv.appendChild(img);
-
-  const title = document.createElement("h1");
-  title.textContent = `#${level.top} ${level.name}`;
-
-  levelDiv.appendChild(imageDiv);
-  levelDiv.appendChild(title);
-
-  container.appendChild(levelDiv);
+    const levelID = document.createElement("h2");
+    levelID.textContent = `${level.id}`
+    
+    levelDiv.appendChild(imageDiv);
+    levelDiv.appendChild(title);
+    
+    container.appendChild(levelDiv);
 }
